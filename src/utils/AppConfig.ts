45c2f211109c17574cade58883a2ab69bde24e8d
John Adib <Mr.John.Adib@gmail.com>
2023-08-13 12:26:40 +0100

    feat: add nextjs

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,7 @@
+export const AppConfig = {
+  site_name: 'Famous Globe',
+  title: 'Famous Globe',
+  description:
+    'Unlock insights into the lives of famous people, celebrities, and influencers from around the globe. Your ultimate source for all things famous and global.',
+  locale: 'en',
+};
