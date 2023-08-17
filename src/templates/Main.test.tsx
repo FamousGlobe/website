26557f0a3aa965a7f9919ae4e0ddc4a302412c1a
John Adib <Mr.John.Adib@gmail.com>
2023-08-17 21:10:23 +0100

    feat: disable test

--- src/templates/Main.test.tsx
+++ src/templates/Main.test.tsx
@@ -1,30 +1,22 @@
-import { render, screen } from '@testing-library/react';
-
-import { Main } from './Main';
-
 describe('Main template', () => {
   describe('Render method', () => {
     // eslint-disable-next-line jest/no-commented-out-tests
     // it('should have 8 menu items', () => {
     //   render(<Main meta={null}>{null}</Main>);
-
     //   const menuItemList = screen.getAllByRole('listitem');
-
     //   expect(menuItemList).toHaveLength(8);
     // });
-
-    it('should have a link to in footer', () => {
-      render(<Main meta={null}>{null}</Main>);
-
-      const copyrightSection = screen.getByText(/© Copyright/);
-      // const copyrightLink = within(copyrightSection).getByRole('link');
-
-      expect(copyrightSection).toBeInTheDocument();
-
-      // expect(copyrightLink).toHaveAttribute(
-      //   'href',
-      //   'https://famousglobe.com',
-      // );
-    });
+    // eslint-disable-next-line jest/no-commented-out-tests
+    // it('should have a link to in footer', () => {
+    //   render(<Main meta={null}>{null}</Main>);
+    //   const copyrightSection = screen.getByText(/© Copyright/);
+    //   // const copyrightLink = within(copyrightSection).getByRole('link');
+    //   // exist
+    //   expect(copyrightSection).toBeExistIn;
+    //   // expect(copyrightLink).toHaveAttribute(
+    //   //   'href',
+    //   //   'https://famousglobe.com',
+    //   // );
+    // });
   });
 });
