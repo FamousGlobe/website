89787306f51caf112f8192cd444d755630a1ef12
John Adib <Mr.John.Adib@gmail.com>
2023-08-18 22:31:39 +0100

    feat: add sample homepage

---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,9 @@
+export default function Homepage() {
+  return (
+    <div className="my-10 flex flex-col items-center text-center align-middle text-xl">
+      <h1>Famous Globe Homepage</h1>
+      <h2>Coming soon...</h2>
+      <time>2023/08/18</time>
+    </div>
+  );
+}
